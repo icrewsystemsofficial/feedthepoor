6b67f20496d378c4e362932f1ef717f8b7e49a8c
Leonard Selvaraja <kashrayks@gmail.com>
2020-08-16 23:43:14 +0530

    blueprint configs

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::get('/invoice/{invoice}/pay', 'PaymentsController@index')->name('payments
 Route::post('/request', 'PaymentsController@request');
 Route::post('/response', 'PaymentsController@response');
 
-Auth::routes();
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::get('/', 'AdminController@index');
