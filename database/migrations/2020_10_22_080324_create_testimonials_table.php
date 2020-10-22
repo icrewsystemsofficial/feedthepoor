c5681ed33cb7dfb75ae6b070ce0557614540a454
Apoorv Pal <apoorvpal01@gmail.com>
2020-10-22 08:44:21 +0530

    Added Testimonial Model and Migration

---
+++ database/migrations/2020_10_22_080324_create_testimonials_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestimonialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->text('message', 50);
+            $table->integer('status');
+            $table->softDeletes('deleted_at', 0);
+            $table->timestamps(
+
+            );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
