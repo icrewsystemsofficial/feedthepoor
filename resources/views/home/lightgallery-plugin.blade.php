66269032b276098460dd1c207cbd532647707819
Anwesha Mohanty <anwesha0322@gmail.com>
2020-11-28 18:27:52 +0530

    created donation gallery UI

---
+++ resources/views/home/lightgallery-plugin.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<head>
+<link type="text/css" rel="stylesheet" href="{{ asset('galleryassets/css/lightgallery.css') }}" />
+</head>
+<body>
+<script src="{{asset('galleryassets/js/lightgallery.min.js')}}"></script>
+
+    <!-- lightgallery plugins -->
+<script src="{{ asset('galleryassets/js/lg-thumbnail.min.js') }}"></script>
+<script src="{{ asset('galleryassets/js/lg-fullscreen.min.js') }}"></script>
+</body>
