de574bc8687b2aa8b3f2d4a683d441a3153b6f7d
Samay Bhattacharyya <51021714+NeilKerman@users.noreply.github.com>
2021-03-22 21:30:34 +0530

    Update guest.blade.php

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,33 +1,45 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-  <div class="container-fluid">
-    <div class="navbar-wrapper">
-    <a class="navbar-brand" href="#pablo">{{ $namePage }}</a>
+<nav class="navbar navbar-expand-md  py-3 navbar-dark bg-white ">
+
+    <a class="navbar-brand text-dark h4" style="margin-left:110px;" href="#"><strong>#feed</strong>ThePoor</a>
+  
+    <div class="collapse navbar-collapse  " id="navbar_example_1">
+        <ul class="navbar-nav ml-auto "  >
+            <li class="nav-item active" >
+                <a class="nav-link text-dark"  href="#"> Who did we feed today? </a>
+            </li>
+           
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-dark" href="#" id="navbar_1_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+                <div class="dropdown-menu" aria-labelledby="navbar_1_dropdown_1">
+                    <a class="dropdown-item text-dark" href="#">About us</a>
+                    <a class="dropdown-item text-dark" href="#">How does it work</a>
+                    <a class="dropdown-item text-dark" href="#">Volunteers</a>
+                    <a class="dropdown-item text-dark" href="#">Partners</a>  
+                    <a class="dropdown-item text-dark" href="#">Testimonials</a>  
+
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-dark" href="#">Contact</a>
+            </li>
+        </ul>
+        <!--button type="button" style="margin-right:150px;" class="btn  btn-primary btn-sm btn-animated btn-animated-y">
+            <span class="btn-inner--visible">Donate Now</span>
+            <span class="btn-inner--hidden"><i class="fas fa-arrow-right"></i></span>
+        </button-->
+        @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a class="btn-sm btn-light" href="{{ route('home') }}">Home</a>
+                    @else
+                        <a class="btn  btn-primary btn-sm btn-animated btn-animated-y" href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a class="btn  btn-primary btn-sm btn-animated btn-animated-y" href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
     </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-bar navbar-kebab"></span>
-      <span class="navbar-toggler-bar navbar-kebab"></span>
-      <span class="navbar-toggler-bar navbar-kebab"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navigation">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a href="{{ route('login') }}" class="nav-link">
-            <i class="now-ui-icons design_app"></i> {{ __("Dashboard") }}
-          </a>
-        </li>
-        <li class="nav-item @if ($activePage == 'register') active @endif">
-          <a href="{{ route('register') }}" class="nav-link">
-            <i class="now-ui-icons tech_mobile"></i> {{ __("Register") }}
-          </a>
-        </li>
-        <li class="nav-item @if ($activePage == 'login') active @endif ">
-          <a href="{{ route('login') }}" class="nav-link">
-            <i class="now-ui-icons users_circle-08"></i> {{ __("Login") }}
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
 </nav>
 <!-- End Navbar -->
