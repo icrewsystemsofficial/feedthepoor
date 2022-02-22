e57dc95f356b7d9d775a30bf1ac4bb57755195c9
Leonard Selvaraja <kashrayks@gmail.com>
2022-02-22 23:18:24 +0530

    Added loading button component

---
+++ resources/views/components/loadingbutton.blade.php
@@ -0,0 +1,32 @@
+
+<script>
+    function loadingButton() {
+        return {
+            loading: false, 
+            disabled: false, 
+
+            clicked() {
+                if(this.loading == false) {
+                    this.loading = true;
+                    this.disabled = true;
+                } else {
+                    this.loading = false;
+                    this.disabled = false;
+                }
+
+
+                setTimeout(() => {
+                    this.clicked();
+                }, 10000);
+            }
+        }
+    }
+</script>
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-primary']) }} x-bind:disabled="disabled" x-data="loadingButton()" @click="clicked">
+    <span x-show="loading">
+        <i class="fa-solid fa-spinner fa-spin"></i>
+        &nbsp;
+    </span>
+    {{ $slot }}
+</button>
