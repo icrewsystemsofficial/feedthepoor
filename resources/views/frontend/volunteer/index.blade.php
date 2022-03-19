5f3f8ac14d95910277a76bf93753f1b396ca0f9f
Kishore <kishorechand473@gmail.com>
2022-03-20 01:00:44 +0530

    made changes in volunteer page

--- resources/views/frontend/volunteer/index.blade.php
+++ resources/views/frontend/volunteer/index.blade.php
@@ -9,7 +9,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 <style>
     .about-hero-header {
-        background-image: linear-gradient(rgba(28, 44, 52, 0.4), rgba(28, 44, 52, 0.4)), url(https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/k-26-aom-19379-olj0127word-1-volunteer_2.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=33836cd64561a589ec978b21287c0597);
+        background-image: linear-gradient(rgba(28, 44, 52, 0.4), rgba(28, 44, 52, 0.4)), url("/volunteer-images/volunteerbackimg.jpg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
@@ -183,6 +183,7 @@
     }
 
 
+
     @keyframes fadeIn {
         from {
             opacity: 0;
@@ -294,60 +295,108 @@
 @endsection
 
 <section class="about-hero-header">
+    <div class="container-fluid pt-11">
+        <div>
+            <h1 class="display-1 text-center text-warning">BECOME A VOLUNTEER</h1>
+        </div>
+        <div>
+            <p class=" d-flex justify-content-center display-4 ms-4 text-white">Volunteerism plays an instrumental role to strengthen civic engagement and safeguards social inclusion.</p>
+            <p class="d-flex justify-content-center display-4 text-white">It also helps to deepen solidarity and solidifies ownership of development results.</p>
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="#form" class="btn btn-warning p-2 pe-3 ps-3" role="button">Join us</a>
+        </div>
+
+    </div>
 </section>
 
-<section class="container mt-5">
-    <div class="d-flex justify-content-center">
-        <h1>Here we have some of our prestigious volunteers</h1>
-    </div>
-    <div class="mt-5">
-        <img class="h-50 mx-auto d-block" src="/volunteer-images/volunteer-1.jpg" alt="volunteer-1">
-    </div>
-    <div class="mt-6">
-        <img class="h-50 mx-auto d-block" src="/volunteer-images/volunteer-2.jpg" alt="volunteer-2">
-    </div>
-    <div class="mt-6">
-        <img class="h-50 mx-auto d-block" src="/volunteer-images/volunteer-3.jpg" alt="volunteer-3">
-    </div>
-    <div class="mt-6">
-        <img class="h-50 mx-auto d-block" src="/volunteer-images/volunteer-4.jpg" alt="volunteer-4">
-    </div>
-    <div class="mt-6">
-        <img class="h-50 w-50 mx-auto d-block" src="/volunteer-images/volunteer-5.jpeg" alt="volunteer-5">
-    </div>
-
-</section>
-
-<section class="section section-lg pt-0 mb-50 mt-6">
-    <div class="container bgcl pb-7">
+<section>
+    <div class="container mt-6 mb-6">
         <div class="row">
-            <div class="col-md-6 slightlyDown">
+            <div class="col mt-6">
+                <h1 class="display-3 mb-3"> <span class="text-warning">#Volunteer</span> with Us</h1>
+                <h4>ROSHNI welcomes people to contribute their skills, time, talent and experience through our Volunteer Programme.</h4>
+                <h4> At ROSHNI we invite volunteers who must be at least 18 years of age and who are willing to commit time as required depending on the available opportunity.</h4>
+            </div>
+            <div class="col">
+                <img src="/volunteer-images/handshakeblue.png">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="bgcl">
+    <div class="d-flex justify-content-center text-white">
+        <h1 class="mt-6">Our <span class="text-warning">prestigious</span> volunteers</h1>
+    </div>
+    <div class="bgcl pe-6 ps-6 pb-6">
+        <div class="row">
+            <div class="mt-6 h-100 w-100 col">
+                <img class="h-50 mx-auto d-block border border-5 border-white" src="/volunteer-images/volunteer-1.jpg" alt="volunteer-1">
+            </div>
+            <div class="mt-6 h-100 w-100 col">
+                <img class="h-50 mx-auto d-block  border border-5 border-white" src="/volunteer-images/volunteer-2.jpg" alt="volunteer-2">
+            </div>
+            <div class="mt-6 h-100 w-100 col">
+                <img class="h-50 mx-auto d-block  border border-5 border-white" src="/volunteer-images/volunteer-3.jpg" alt="volunteer-3">
+            </div>
+        </div>
+        <div class="row">
+            <div class="mt-6 h-100 w-100 col">
+                <img class="h-50 mx-auto d-block  border border-5 border-white" src="/volunteer-images/volunteer-4.jpg" alt="volunteer-4">
+            </div>
+            <div class="mt-6 h-100 w-100 col">
+                <img class="h-50 mx-auto d-block  border border-5 border-white" src="/volunteer-images/volunteer-6.jpg" alt="volunteer-1">
+            </div>
+            <div class="mt-6 h-75 w-100 col">
+                <img class="h-75 mx-auto d-block  border border-5 border-white" src="/volunteer-images/volunteer-5.jpg" alt="volunteer-5">
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section>
+    <div class="container mt-5 mb-6">
+        <div class="d-flex justify-content-center mb-3">
+            <h1 class="display-3">#Raise your hands to <span class="text-warning">help</span> the poor</h1>
+        </div>
+        <div class="d-flex justify-content-center">
+            <img class="w-50" src="/volunteer-images/volunteerrise.png" alt="">
+        </div>
+    </div>
+</section>
+
+<section class="section section-lg pt-0 mt-6">
+    <div class="container-fluid bgcl pb-7">
+        <div class="row">
+            <div class="col-md-5 slightlyDown">
                 <img src="https://images.assetsdelivery.com/compings_v2/dizanna/dizanna1807/dizanna180700148.jpg" class="about-image boxShadow">
             </div>
-            <div class="col-md-6 mt-6">
+            <div class="col-md-7 mt-6">
                 <div class="heading">
                     <h2 class="heading-title">
                         <span class="heading-title-main text-white">
-                            #joinWithUsToFeedThePoor
+                            #joinWithUsTo <span class="text-warning">FeedThePoor</span>
                         </span>
                     </h2>
                 </div>
                 <div class="content text-white">
-                    Share your helping hands with us! volunteers do not necessarily have the time; they just have the heart. You make a living by what you get. You make a life by what you give. Volunteers are the only human beings on the face of the earth who reflect this nation's compassion, unselfish caring, patience, and just plain loving one another. Our motive is to feed the underpivileged half of our nation, that is heavily affeccted by the pangs of poverty and isn't capable of earning their bread. Let's join our hands together to make our country hunger free</p>
+                    Share your helping hands with us! volunteers do not necessarily have the time. they just have the heart. You make a living by what you get. You make a life by what you give. Volunteers are the only human beings on the face of the earth who reflect this nation's compassion, unselfish caring, patience, and just plain loving one another. Our motive is to feed the underpivileged half of our nation, that is heavily affeccted by the pangs of poverty and isn't capable of earning their bread. Let's join our hands together to make our country hunger free</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-<section class="container mt-3">
-    <h1 class="d-flex justify-content-center">VOLUNTEER WITH US</h1>
-    <h3 class="d-flex justify-content-center"># Become a hunger hero</h3>
-    <div class="mt-6">
-        <form method="POST" enctype="multipart/form-data" action="#">
+<section class="container mb-6">
+    <h1 class="d-flex justify-content-center">APPLY HERE TO &nbsp;<span class="text-warning">VOLUNTEER</span>&nbsp; WITH US</h1>
+    <h3 class="d-flex justify-content-center text-warning"># Become a hunger hero</h3>
+    <div class="mt-6 bgcl pt-5 pe-5 ps-5 border border-5 border-dark rounded-3">
+        <form method="POST" enctype="multipart/form-data" action="#" id="form">
             @csrf
             <div>
-                <h3>Personal details</h3>
+                <h3 class="text-warning">Personal details</h3>
             </div>
             <div class="row">
                 <div class="col form-floating mb-3 mt-3">
@@ -372,7 +421,7 @@
             </div>
             <br><br>
             <div>
-                <h3>Contact details</h3>
+                <h3 class="text-warning">Contact details</h3>
             </div>
 
             <div class="row">
@@ -389,23 +438,23 @@
             <br><br>
 
             <div>
-                <h3>How often will you be able to help</h3>
+                <h3 class="text-warning">How often will you be able to help</h3>
             </div>
 
             <div class="row mt-4 ms-2">
-                <div class="col form-check">
+                <div class="col form-check text-white">
                     <input type="radio" class="form-check-input" id="radio1" value="option1">Daily
                     <label class="form-check-label"></label>
                 </div>
-                <div class="col form-check me-6">
+                <div class="col form-check me-6 text-white">
                     <input type="radio" class="form-check-input" id="radio2" value="option2">Multiple days in a week
                     <label class="form-check-label"></label>
                 </div>
-                <div class="col form-check ms-4">
+                <div class="col form-check ms-4 text-white">
                     <input type="radio" class="form-check-input" id="radio3" value="option3">Once a week
                     <label class="form-check-label"></label>
                 </div>
-                <div class="col form-check">
+                <div class="col form-check text-white">
                     <input type="radio" class="form-check-input" id="radio4" value="option4">Twice a week
                     <label class="form-check-label"></label>
                 </div>
@@ -413,7 +462,7 @@
             <br><br>
 
             <div>
-                <h3>Your Location</h3>
+                <h3 class="text-warning">Your Location</h3>
             </div>
 
             <div class="form-floating mb-3 mt-4">
@@ -465,7 +514,7 @@
                 </div>
             </div>
             <div class="d-flex justify-content-center mt-4 mb-6">
-                <button type="submit" class="btn btn-secondary">Submit</button>
+                <button type="submit" class="btn btn-warning">Submit</button>
             </div>
         </form>
     </div>
