62aad5626b2de972241133fdb4b97646a1e57564
Leonard Selvaraja <kashrayks@gmail.com>
2022-03-24 02:05:39 +0530

    [Review] Volunteer page. Rework needed, added links to footer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,9 +25,9 @@
 
    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
 
-   <script src="{{ asset('js/bootstrap.min.js') }}" />
+   <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
-   {{-- APP's COMPILED CSS? --}}
+   {{-- APPs COMPILED CSS? --}}
    <link href="{{ asset('adminkit/static/css/app.css') }}" rel="stylesheet">
 
 
