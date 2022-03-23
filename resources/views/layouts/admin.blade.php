03944227ffff924332ec565735b0b7cce60c9a3f
Leonard Selvaraja <kashrayks@gmail.com>
2022-03-24 03:10:33 +0530

    [Bug fix] FAQ Helper & Seeder

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,6 @@
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
    <meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
    <meta name="author" content="icrewsystems">
-   <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
    <link rel="preconnect" href="https://fonts.gstatic.com">
    <link rel="shortcut icon" href="{{ asset('adminkit/static/img/icons/icon-48x48.png') }}" />
    {{--
