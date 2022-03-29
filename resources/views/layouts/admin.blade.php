8f0229d097c16d9e920cb375fbd3fd45c9b543aa
Kishore <kishorechand473@gmail.com>
2022-03-29 23:46:38 +0530

    made changes in admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,11 +16,8 @@
 
    {{-- <link rel="stylesheet" href="https://demo.adminkit.io/css/dark.css"> --}}
    {{-- BOOTSTRAP 5 CDN --}}
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
 
    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}" />
-
-   {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
 
    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
@@ -29,13 +26,13 @@
 
 
    {{-- FONTAWESOME --}}
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
    <link href="{{ asset('css/fontawesome.css') }}" rel="stylesheet" type="text/css">
 
 
    {{-- WEBSITE FONT --}}
-   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+   <link href="{{ asset('css/websitefont.css') }}" rel="stylesheet" type="text/css">
+
    <style>
       [x-cloak] {
          display: none !important;
