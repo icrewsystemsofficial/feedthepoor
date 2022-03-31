12df3c9e6b239e586fdac277f52a63aeebb220dd
SATHISHKUMAR <sathishprakash233@gmail.com>
2022-03-31 23:11:58 +0530

    receipt generation completed

--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,8 @@ Route::name('frontend.')->group(function () {
     Route::get('/faq', [HomeController::class, 'faq'])->name('faq');
     Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
     Route::post('/contact', [HomeController::class, 'savecontact'])->name('savecontact');
+    
+    Route::get('/receipt', [HomeController::class, 'receipt'])->name('receipt');
 });
 
 /*
