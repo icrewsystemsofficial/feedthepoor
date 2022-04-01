0bdcfd3f30686a46227deaef9b2c4328f0c341b1
AnushaShivakumar <anushagsk01@gmail.com>
2022-04-02 04:27:32 +0530

    Partners page

--- resources/views/frontend/partners/index.blade.php
+++ resources/views/frontend/partners/index.blade.php
@@ -277,6 +277,461 @@ h1 {
     }
 }
 
+/* ---------------------------Partners------------------ */
+
+.hero {
+    position: relative; 
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.hero::before {    
+      content: "";
+      background-image: url('https://images.pexels.com/photos/3184422/pexels-photo-3184422.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+      background-size: cover;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      bottom: 0px;
+      left: 0px;
+      /* opacity: 0.8; */
+}
+
+h1 {
+  position: relative;
+  color: #8e6c59;  
+  font-size: 12rem;
+  line-height: 0.9;
+  text-align: right;
+}
+
+/* ------------------------NAVBAR---------------------------- */
+#mainNavbar {
+    font-size: 1.5rem;
+    font-weight: 100;    
+}
+
+#mainNavbar .nav-link {
+    color: white;
+}
+
+#mainNavbar .nav-link:hover {
+    color: #b29c8f;
+}
+
+#mainNavbar .navbar-brand {
+    color: #b29c8f;
+    font-size: 1.5rem;
+}
+
+
+.navbar.scrolled {
+    background: #141e30;
+    transition: background 500ms;
+}
+
+@media (max-width: 1200px) {
+    #headingGroup h1 {
+        font-weight: 100;
+        font-size: 3rem;
+    }
+    .blurb h2 {
+        font-size: 2rem;
+    }
+}
+
+/* --------------------MAIN BOX------------------------ */
+
+
+* {
+	box-sizing: border-box;
+}
+body {
+	font-family: sans-serif;
+	line-height: 1.6;
+	margin: 0;
+	min-height: 100vh;
+    margin:0;
+  padding:0;
+  background: linear-gradient(#141e30, #243b55);
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+
+.a {
+	text-decoration: none;
+  color: #34495e;
+  padding: 10px 15px;
+	text-transform: uppercase;
+	text-align: center;
+	display: block;
+  color: #34495e;
+	font-size: .99em;
+}
+
+.main-nav a:hover {
+	color: #718daa;
+}
+
+@media (min-width: 1025px) {
+	.header {
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
+} */
+
+html {
+  height: 100%;
+}
+.square {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 400px;
+  padding: 65px;
+  transform: translate(-50%, -50%);
+  background: rgba(0,0,0,.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+  border-radius: 10px;
+}
+.square form a{
+    position: relative;
+    display: inline-flexbox;
+    padding: 20px 20px;
+    color: #f8edeb;
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    /* overflow: hidden; */
+    /* transition: .5s; */
+    margin-top: 40px;
+    letter-spacing: 4px
+}
+
+.square a:hover {
+  background: #f8edeb;
+  color: #494e5b;
+  border-radius: 5px;
+  box-shadow: 0 0 5px #f8edeb,
+              0 0 25px #f8edeb,
+              0 0 50px #f8edeb,
+              0 0 100px #f8edeb;
+}
+
+.square a span {
+  position: absolute;
+  display: block;
+  /* justify-content: center; */
+}
+.square a span:nth-child(1) {
+  top: 0;
+  left: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(90deg, transparent, #f8edeb);
+  animation: btn-anim1 1s linear infinite;
+}
+
+@keyframes btn-anim1 {
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+}
+
+ .square a span:nth-child(2) {
+  top: -100%;
+  right: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(180deg, transparent, #f8edeb);
+  animation: btn-anim2 1s linear infinite;
+  animation-delay: .25s
+} 
+
+@keyframes btn-anim2 {
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+}
+
+.square a span:nth-child(3) {
+  bottom: 0;
+  right: -100%;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(270deg, transparent, #f8edeb);
+  animation: btn-anim3 1s linear infinite;
+  animation-delay: .5s
+}
+
+@keyframes btn-anim3 {
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+}
+
+.square a span:nth-child(4) {
+  bottom: -100%;
+  left: 0;
+  width: 2px;
+  height: 100%;
+  background: linear-gradient(360deg, transparent, #f8edeb);
+  animation: btn-anim4 1s linear infinite;
+  animation-delay: .75s
+}
+
+@keyframes btn-anim4 {
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+}
+
+.fontchange {
+  color: #b29c8f;
+
+}
+
+.paragraph {
+  text-align: justify;
+}
+
+.line {
+  padding: 5% 0 5% 0%; 
+  font-family: 'Hurricane', cursive; 
+  font-weight: 100;
+  font-size: 100em;
+    color: #b29c8f;
+
+}
+
+
+/* ----------------------------SLIDE BAR----------------------- */
+
+
+h2{
+  text-align:center;
+  padding: 20px;
+  color: #b29c8f;
+}
+
+.slick-prev, .slick-next {
+  position: absolute;
+  top: 135%;
+  font-size: 1.8rem;
+}
+
+.slick-prev {
+  left: 0;
+}
+
+.slick-next {
+  right: 0;
+}
+
+.slick-slider {
+  position: relative;
+  display: block;
+  box-sizing: border-box;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+      user-select: none;
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -ms-touch-action: pan-y;
+      touch-action: pan-y;
+  -webkit-tap-highlight-color: transparent;
+}
+
+.slick-list {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+
+.slick-list:focus {
+  outline: none;
+}
+
+.slick-list.dragging {
+  cursor: pointer;
+  cursor: hand;
+}
+
+.slick-slider .slick-track,
+.slick-slider .slick-list {
+    -webkit-transform: translate3d(0, 0, 0);
+       -moz-transform: translate3d(0, 0, 0);
+        -ms-transform: translate3d(0, 0, 0);
+         -o-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+}
+
+.slick-track {
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+}
+
+.slick-track:before,
+.slick-track:after {
+    display: table;
+    content: '';
+}
+
+.slick-track:after {
+    clear: both;
+}
+
+.slick-loading .slick-track {
+    visibility: hidden;
+}
+
+.slick-slide {
+    display: none;
+    float: left;
+    height: 100%;
+    min-height: 1px;
+}
+
+[dir='rtl'] .slick-slide {
+    float: right;
+}
+
+.slick-slide img {
+    display: block;
+}
+
+.slick-slide.slick-loading img {
+    display: none;
+}
+
+.slick-slide.dragging img {
+    pointer-events: none;
+}
+.slick-initialized .slick-slide {
+    display: block;
+}
+.slick-loading .slick-slide {
+    visibility: hidden;
+}
+.slick-vertical .slick-slide {
+    display: block;
+    height: auto;
+    border: 1px solid transparent;
+}
+
+.slick-arrow.slick-hidden {
+    display: none;
+}
+
+.slide {
+    transition: filter .4s;
+    margin: 0px 40px;
+}
+
+.fas {
+    color: #b29c8f;
+}
+.slick-prev {
+    left: 0;
+}
+.slick-prev, .slick-next {
+    position: absolute;
+    top: 35%;
+    font-size: 1.8rem;
+}
+
+.section {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.outline {
+  padding-bottom: 5%;
+}
+
+/* ------------------------------FORM--------------------------- */
+
+* {
+  box-sizing: border-box;
+}
+
+
+.container {
+  padding: 16px;
+  background-color: #b29c8f;
+  border-radius: 2%;
+  opacity: 9;
+ 
+}
+
+.formfont {
+font-family: sans-serif;
+font-weight: 10;
+font-size: x-large;
+
+}
+
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f8edeb;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+.submit {
+  background-color: #243b55;
+  color: white;
+  padding: 16px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.registerbtn:hover {
+  opacity: 1;
+}
+
+
 /* --------------------MAIN BOX------------------------ */
 
 
@@ -780,8 +1235,133 @@ $(function () {
     });
 });
 
+
+$(document).ready(function () {
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows: true,
+        dots: false,
+        pauseOnHover: false,
+        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
+
 </script>
 @endsection
 
 
-<section></section>
+<section>
+
+
+    <div style="padding: 0%;" class="hero container">
+        <h1 class="top-left"></h1>
+    </div>
+
+    <div class="square">
+        <form>
+            <a href="#">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Be our partners
+            </a>
+        </form>
+    </div>
+
+    <div style="color: aliceblue; padding: 5% 0 5% 0;" class="text-center justify-content">
+        <h3>Re-imagining how the web works</h3>
+    </div>
+
+    <section style="padding: 0 10% 2% 10%;">
+        <div class="row align-items-center content">
+            <div class="col-md-6 text-center">
+                <div class="row justify-content-center">
+                    <div class="col-10 col-lg-8 blurb mb-5 mb-md-0 fontchange">
+                        <h2>Transform your business digitally and unlock it's full potential</h2>
+                        <img src="imgs/lolli_icon.png" alt="" class="d-none d-lg-inline">
+                        <p class="lead paragraph">From desinging to deploying, we offer comprehensive range of full
+                            stack
+                            development tailored to our client’s business.
+                            With the help of advanced techologies, we help you to expand your horizons in your field of
+                            work.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img src="https://images.pexels.com/photos/7688164/pexels-photo-7688164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    alt="" class="img-fluid">
+            </div>
+        </div>
+    </section>
+
+
+    <h2>Our Clients</h2>
+    <section class="trigger section gutter-horizontal bg-gray gutter-vertical--m gutter-horizontal outline">
+        <div class="customer-logos slider">
+            <div class="slide-in-right slide"><img
+                    src="https://th.bing.com/th/id/OIP.e0WkjofvUMmsYRMDtud4NgHaCH?pid=ImgDet&rs=1"
+                    alt="Kinderhotel Aschauerhof" height="150" width="150"></a></div>
+            <div class="slide-in-right slide"><img
+                    src="https://t3.ftcdn.net/jpg/00/97/16/98/500_F_97169893_o9dpYDfiYgxcI8OiAK377MrAdVwjomiS.jpg"
+                    height="150" width="150"></a></div>
+            <div class="slide-in-right slide"><img
+                    src="https://images.yourstory.com/cs/images/companies/logo-1586419567684.jpg?fm=png&auto=format&w=400&h=400&fit=fill&fill=solid"
+                    height="150" width="150"></a></div>
+    </section>
+
+    <div class="container">
+        <form action="">
+            <div class="container">
+                <h1 class="formfont">Join hands to make a difference</h1>
+                <p class="formfont">Please fill in this form to create an account.</p>
+                <hr>
+
+                <label class="formfont" for="org"><b>Organization Details</b></label>
+                <input type="text" placeholder="Registered entity name" name="Details" required>
+
+                <input type="text" placeholder="Registered Address" name="Details" required>
+
+                <label class="formfont" for="contact"><b>Primary contact</b></label>
+                <input type="text" placeholder="Full name" name="name" required>
+                <input type="text" placeholder="Email id" name="email" required>
+                <hr>
+                <button type="submit" class="submit">Submit</button>
+            </div>
+
+        </form>
+    </div>
+    <br><br>
+
+
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
+    <script type="text/javascript" src="frontend.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</section>
