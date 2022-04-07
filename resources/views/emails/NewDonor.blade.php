0d53787d884e98b9b32c479550d3ea84473fc0ef
Leonard Selvaraja <kashrayks@gmail.com>
2022-04-07 20:13:48 +0530

    Welcome email, added phone number

--- resources/views/emails/NewDonor.blade.php
+++ resources/views/emails/NewDonor.blade.php
@@ -1,10 +1,11 @@
 @component('mail::message')
 # Hey {{ $user->name }},
-We cordially welcome you to the family of Roshni Moolchandani Charitable Trust, where we work to bring smiles to the underprivileged.
+We cordially welcome you to the family of {{ config('app.ngo_name') }}, where we work to bring smiles to the underprivileged.
 <br><br>
-Your dedicated relationship manager is Ms. XYZ (+91 9999999999).
+Your dedicated relationship manager is Ms. Neha (+91 95831 86287). Please contact her in regards to any queries you have
+with your donation.
 <br><br>
-Every second you spend with us matters and so does the penny that you donate. Our motto is to eradicate hunger and you can be our envoy. 
+Every second you spend with us matters and so does the penny that you donate. Our motto is to eradicate hunger and you can be our envoy.
 <br><br>
 So think no more and click the button below to become a reason someone sleeps with a smile.
 @component('mail::button', ['url' => route('frontend.donate')])Donate Now @endcomponent
