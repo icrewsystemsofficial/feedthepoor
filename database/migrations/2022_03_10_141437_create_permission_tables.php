fa8c77444df25423f24085bfb545657fd4b72e4d
Leonard Selvaraja <kashrayks@gmail.com>
2022-05-09 21:19:10 +0100

    Production changes

--- database/migrations/2022_03_10_141437_create_permission_tables.php
+++ database/migrations/2022_03_10_141437_create_permission_tables.php
@@ -40,8 +40,8 @@ class CreatePermissionTables extends Migration
                 $table->unsignedBigInteger($columnNames['team_foreign_key'])->nullable();
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
-            $table->string('name');       // For MySQL 8.0 use string('name', 125);
-            $table->string('guard_name'); // For MySQL 8.0 use string('guard_name', 125);
+            $table->string('name', 125);       // For MySQL 8.0 use string('name', 125);
+            $table->string('guard_name', 125); // For MySQL 8.0 use string('guard_name', 125);
             $table->timestamps();
             if ($teams || config('permission.testing')) {
                 $table->unique([$columnNames['team_foreign_key'], 'name', 'guard_name']);
