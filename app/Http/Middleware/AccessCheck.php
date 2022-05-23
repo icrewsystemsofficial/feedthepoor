80183693ddcc9ed75a10a0dba49f258b69fec53e
Thirumalai15 <m.thirurahul@gmail.com>
2022-05-23 23:01:13 +0530

    Added: module access control

---
+++ app/Http/Middleware/AccessCheck.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ModuleAccess;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class AccessCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        $route_name = Route::current()->getName();
+
+        $module_access = ModuleAccess::whereJsonContains('module_route_path', $route_name)->first();
+//        dd($module_access);
+//        $user = auth()->user()->getAllPermissions();
+//            dd($user);
+        $allowed_perms = json_decode($module_access->permissions_that_can_access);
+
+        foreach ($allowed_perms as $permission) {
+            if (!auth()->user()->can($permission)) {
+                abort(403);
+            }
+        }
+        return $next($request);
+
+    }
+}
