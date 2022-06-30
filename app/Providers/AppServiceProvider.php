fa5ee02b5bd2fc0153404e8d91707e0335e5d212
Leonard Selvaraja <kashrayks@gmail.com>
2022-07-01 04:33:01 +0530

    Added Discord Failing webhook entry, Added new provider for failed jobs discord notification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,14 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Queue\Events\JobFailed;
-use Illuminate\Support\Facades\Queue;
+
 use ConsoleTVs\Charts\Registrar as Charts;
 use App\Charts\DailyProcurement;
 
-use App\Helpers\NotificationHelper;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -36,56 +32,5 @@ class AppServiceProvider extends ServiceProvider
         $charts->register([
             DailyProcurement::class,
         ]);
-        Queue::failing(function (JobFailed $event) {
-            
-            Http::post(env('DISCORD_WEBHOOK_URL'), [
-                'embeds' => 
-                    [
-                        [
-                            'title' => '[FeedThePoor] Job failed',
-                            'description' => 'Job failed: ' . $event->connectionName . ' - ' . $event->job->getName(),
-                            'color' => '7506394',
-                            'timestamp' => now()->toISOString(),
-                            'fields' => [
-                                [
-                                    'name' => 'Message',
-                                    'value' => $event->exception->getMessage(),
-                                    'inline' => false,
-                                ],
-                                [
-                                    'name' => 'File',
-                                    'value' => $event->exception->getFile(),
-                                    'inline' => false,
-                                ],
-                                [
-                                    'name' => 'Class',
-                                    'value' => $event->job->resolveName(),
-                                    'inline' => false,
-                                ],
-                                [
-                                    'name' => 'Attempts',
-                                    'value' => $event->job->attempts(),
-                                    'inline' => true,
-                                ],                                
-                                [
-                                    'name' => 'Line',
-                                    'value' => $event->exception->getLine(),
-                                    'inline' => true,
-                                ],                                
-                            ],                            
-                        ]
-                    ]
-            ]);        
-
-            app(NotificationHelper::class)->notifyAllAdmins('Job failed', 'Job failed: ' . $event->connectionName . ' - ' . $event->job->getName() . ' - ' . $event->exception->getMessage(), 'APP');
-
-            if ($event->job->attempts() == 2) {
-                $event->job->delete();
-            }
-            else{
-                $event->job->release(5 * $event->job->attempts());
-            }
-
-        });
     }
 }
